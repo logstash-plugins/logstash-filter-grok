068ba34d7089c617203f4a14db9ce2ed05f0f984
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-03-03 16:35:45 +0100

    workaround wrong loading path when testing from logstash project
    Fixes #18

--- spec/filters/grok_spec.rb
+++ spec/filters/grok_spec.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
-require 'spec_helper'
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/filters/grok"
 
 describe LogStash::Filters::Grok do
 
--- spec/spec_helper.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-require "logstash/devutils/rspec/spec_helper"
-require "logstash/filters/grok"
-
-PLUGIN_HOME = ::File.expand_path(::File.join(::File.dirname(__FILE__), ".."))
