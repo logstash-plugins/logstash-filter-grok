2cdc05aa8cda75c52410dee9193e773ad276d183
Joao Duarte <jsvduarte@gmail.com>
2015-05-05 19:01:59 +0100

    set LOGSTASH_HOME in specs if not defined

--- spec/filters/grok_spec.rb
+++ spec/filters/grok_spec.rb
@@ -1,5 +1,13 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
+
+# running the grok code outside a logstash package means
+# LOGSTASH_HOME will not be defined, so let's set it here
+# before requiring the grok filter
+unless LogStash::Environment.const_defined?(:LOGSTASH_HOME)
+  LogStash::Environment::LOGSTASH_HOME = File.expand_path("../../../", __FILE__)
+end
+
 require "logstash/filters/grok"
 
 describe LogStash::Filters::Grok do
