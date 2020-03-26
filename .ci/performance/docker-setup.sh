ec8f7ed658d0923c044e003a6c595b29a3a3efa8
Joao Duarte <jsvduarte@gmail.com>
2020-03-26 14:16:51 +0000

    update performance tests path to .ci/performance/

--- .ci/performance/docker-setup.sh
+++ .ci/performance/docker-setup.sh
@@ -3,7 +3,5 @@
 # This is intended to be run inside the docker container as the command of the docker-compose.
 set -ex
 
-cd .ci
-
 # docker will look for: "./docker-compose.yml" (and "./docker-compose.override.yml")
-./docker-setup.sh
+.ci/docker-setup.sh
