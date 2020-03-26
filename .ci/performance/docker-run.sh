e448a22afb60bceeb2d7eec79f276b602dbba9dd
Joao Duarte <jsvduarte@gmail.com>
2020-03-26 13:24:30 +0000

    update performance tests path to .ci/performance/

--- .ci/performance/docker-run.sh
+++ .ci/performance/docker-run.sh
@@ -3,8 +3,6 @@
 # This is intended to be run inside the docker container as the command of the docker-compose.
 set -ex
 
-CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
-
 cd .ci
 
 # docker will look for: "./docker-compose.yml" (and "./docker-compose.override.yml")
